dbd1379c70d5bf27d4803018171789ad8622532a
John Maurick jazmin <maurickjazmin547@gmail.com>
2025-12-01 16:15:13 +0800

    Update rooms.js

--- src-20251015T092429Z-1-001 (1)/src/routes/rooms.js
+++ src-20251015T092429Z-1-001 (1)/src/routes/rooms.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {getRooms, getRoom, createRoom, updateRoom, deleteRoom, getAvailableRooms, getRoomBookings} = require('../controllers/roomController');
+const { getBookings, getBooking, createBooking, updateBooking, deleteBooking } = require('../controllers/bookingController');
 
-// Available rooms endpoint (must come before /:id)
-router.get('/available', getAvailableRooms);
-
-// Standard CRUD
-router.route('/').get(getRooms).post(createRoom);
-
-// Room bookings history
-router.get('/:id/bookings', getRoomBookings);
-
-// Single room operations
-router.route('/:id').get(getRoom).put(updateRoom).delete(deleteRoom);
+router.route('/').get(getBookings).post(createBooking);
+router.route('/:id').get(getBooking).put(updateBooking).delete(deleteBooking);
 
 module.exports = router;
-
-
