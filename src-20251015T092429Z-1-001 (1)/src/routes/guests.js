ef4df1cc7fff1d1d78d5d17ee8aa7d24953a5499
John Maurick jazmin <maurickjazmin547@gmail.com>
2025-12-03 16:58:36 +0800

    Delete src-20251015T092429Z-1-001 (1)/src directory

--- src-20251015T092429Z-1-001 (1)/src/routes/guests.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBookings, getBooking, createBooking, updateBooking, deleteBooking } = require('../controllers/bookingController');
-
-router.route('/').get(getBookings).post(createBooking);
-router.route('/:id').get(getBooking).put(updateBooking).delete(deleteBooking);
-
-module.exports = router;
